01d5ece665fe63ad64899474b8896005c9b73980
Peter Guo <peterpeiguo@gmail.com>
2018-09-04 14:49:53 -0700

    pass function as parmeters

--- common_db.ps1
+++ common_db.ps1
@@ -39,3 +39,43 @@ Function Query-Database {
 
     $result
 }
+
+#a more generic version of the above, it takes a function used to process result
+Function Query-Database-And-Process {
+    Param($server, $database, $user, $password, $query, $process_function, [ref]$result)
+
+    Try {
+        $sqlConnection = New-Object System.Data.SqlClient.SqlConnection
+        $sqlConnection.ConnectionString = "Server={0};Database={1};UID={2};PWD={3}" -f $server, $database, $user, $password
+
+        $sqlCommand = New-Object System.Data.SqlClient.SqlCommand
+        $sqlCommand.CommandText = $query
+        $sqlCommand.Connection = $sqlConnection
+
+        $sqlAdapter = New-Object System.Data.SqlClient.SqlDataAdapter
+        $sqlAdapter.SelectCommand = $sqlCommand
+
+        $data = New-Object System.Data.DataSet
+
+        $sqlAdapter.Fill($data) > $null
+        $process_function.Invoke($data, $result.value)
+    } Catch {
+        echo $_.Exception
+        $result.value.Exception = $_.Exception
+    } Finally {
+        if ($data) {
+            $data.Dispose()
+        }
+        if ($sqlAdapter) {
+            $sqlAdapter.Dispose()
+        }
+        if ($sqlConnection) {
+            $sqlConnection.Dispose()
+        }
+        if ($sqlCommand) {
+            $sqlCommand.Dispose()
+        }
+    }
+
+    $result
+}
